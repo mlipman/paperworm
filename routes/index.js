b5297c022049f46aa1fd971566dd024925dbfbfc
Michael Lipman <michael.b.lipman@gmail.com>
2014-02-11 00:53:08 +0000

    Main changes to index.js and index.handlebars to allow for dynamic insertion. index.js is pulling from data.json which is now partially populated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
-// Get all of our friend data
+// data has all the data
+var data = require("../data.json");
 
-exports.view = function(req, res){
-	res.render('index');
+
+exports.view = function(req, res) {
+	// we will build a paper specific json object called info for this paper only
+	var scadsHash = data["scads"];
+
+	var info = scadsHash;
+
+
+	res.render('index', info);
 };
