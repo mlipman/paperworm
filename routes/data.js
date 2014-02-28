f47145177075c279d9ab72a1213f9b60085a465e
Gina Pai <ginapai@gmail.com>
2014-02-28 06:14:12 +0000

    still working on DB

--- routes/data.js
+++ routes/data.js
@@ -5,7 +5,7 @@ exports.serializedString = function(req, res) {
 	var paper = req.params.paper;
 	models.Papers.find({"details.name" : paper}).exec(afterQuery);
 	function afterQuery(err, myresult){
-		var serString = myresult[0].serializedString;
+		var serString = myresult.serializedString;
 		var hash = {};
 		hash["val"] = serString;
 		res.render('read', hash)
